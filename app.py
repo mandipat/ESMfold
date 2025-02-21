8cb738da388287f6ae2960051e8a61d0c3f9f903
mandipat <mandipat@usc.edu>
2025-02-20 16:52:01 -0800

    with style options

--- app.py
+++ app.py
@@ -11,16 +11,20 @@ import biotite.structure.io as bsio
 st.sidebar.title('🎈 ESMFold')
 st.sidebar.write('[*ESMFold*](https://esmatlas.com/about) is an end-to-end single sequence protein structure predictor based on the ESM-2 language model. For more information, read the [research article](https://www.biorxiv.org/content/10.1101/2022.07.20.500902v2) and the [news article](https://www.nature.com/articles/d41586-022-03539-1) published in *Nature*.')
 
-# stmol
-def render_mol(pdb):
-    pdbview = py3Dmol.view()
-    pdbview.addModel(pdb,'pdb')
-    pdbview.setStyle({'cartoon':{'color':'spectrum'}})
-    pdbview.setBackgroundColor('white')#('0xeeeeee')
-    pdbview.zoomTo()
-    pdbview.zoom(2, 800)
-    pdbview.spin(True)
-    showmol(pdbview, height = 500,width=800)
+
+style_options = ["cartoon", "stick", "sphere", "line"]
+selected_style = st.selectbox("Select style", style_options, index=0)
+bg_color = st.color_picker("Background Color", "#FFFFFF")
+spin = st.checkbox("Spin", value=True)
+
+def render_mol(pdb_string):
+    view = py3Dmol.view()
+    view.addModel(pdb_string, 'pdb')
+    view.setStyle({selected_style: {'color': 'spectrum'}})
+    view.setBackgroundColor(bg_color)
+    view.zoomTo()
+    view.spin(spin)
+    showmol(view, height=500, width=800)
 
 # Protein sequence input
 DEFAULT_SEQ = "MGSSHHHHHHSSGLVPRGSHMRGPNPTAASLEASAGPFTVRSFTVSRPSGYGAGTVYYPTNAGGTVGAIAIVPGYTARQSSIKWWGPRLASHGFVVITIDTNSTLDQPSSRSSQQMAALRQVASLNGTSSSPIYGKVDTARMGVMGWSMGGGGSLISAANNPSLKAAAPQAPWDSSTNFSSVTVPTLIFACENDSIAPVNSSALPIYDSMSRNAKQFLEINGGSHSCANSGNSNQALIGKKGVAWMKRFMDNDTRYSTFACENPNSTRVSDFRTANCSLEDPAANKARKEAELAAATAEQ"
